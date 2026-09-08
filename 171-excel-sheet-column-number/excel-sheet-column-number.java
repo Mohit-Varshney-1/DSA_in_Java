bd36b3abc2719ddcbb3c00315d825d65a13a31ff
Mohit-Varshney-1 <mohitvarshney549@gmail.com>
2026-09-08 22:01:50 +0530

    Time: 1 ms (90.11%) | Memory: 43.5 MB (72.56%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+      int r = 0;
+      for(int i = 0; i < columnTitle.length(); i++){
+        int value = columnTitle.charAt(i) - 'A' + 1;
+        // ye basic formula h is question ka
+        r = r*26 + value;
+      }  
+      return r;
+    }
+}
